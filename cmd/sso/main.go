ba95bbb5f742b1db495be4d76f1b3992078cc302
dmitrii <dmitrii@domain.com>
2024-02-22 23:45:37 +0300

    Add logger

--- cmd/sso/main.go
+++ cmd/sso/main.go
@@ -2,7 +2,16 @@ package main
 
 import (
 	"fmt"
+	"log/slog"
+	"os"
 	"sso/internal/config"
+	"sso/internal/lib/logger/handlers/slogpretty"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
 )
 
 func main() {
@@ -11,9 +20,53 @@ func main() {
 	fmt.Println(cfg)
 
 	// TODO логгер
+	log := setupLogger(cfg.Env)
+	log.Info(
+		"starting application",
+		slog.String("env", cfg.Env),
+		slog.Int("port", cfg.GRPC.Port),
+		slog.Any("configuration", cfg),
+	)
 
 	// TODO инициализация приложения (app)
 
 	// TODO запустить gRPC-сервер приложения
 
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	// вид лога зависит от окружения
+	switch env {
+	case envLocal:
+		//// текст удобен при просмотре
+		//log = slog.New(
+		//    slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		//)
+		log = setupPrettySlog()
+	case envDev:
+		// json удобен для метрик
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+
+	return log
+}
+
+func setupPrettySlog() *slog.Logger {
+	opts := slogpretty.PrettyHandlerOptions{
+		SlogOpts: &slog.HandlerOptions{
+			Level: slog.LevelDebug,
+		},
+	}
+
+	handler := opts.NewPrettyHandler(os.Stdout)
+
+	return slog.New(handler)
 }
